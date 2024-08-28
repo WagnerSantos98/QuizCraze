93194da57031ca0255f6978d867ae02b18696718
WagnerSantos98 <wagner.jesus98@outlook.com>
2024-08-28 13:20:52 -0300

    feat: implementação de teste

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,2 +1,14 @@
-import { useContext, useEffect } from 'react'
-import { QuizContext } from './context/quiz'
+//import { useContext, useEffect } from 'react'
+//import { QuizContext } from './context/quiz'
+
+import './assets/css/style.css'
+
+function App(){
+    return(
+        <div className="App">
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+
+export default App;
